c2018f83f5727ea9f8561db7d74068f854d74c75
Nathan Wiebe <nathan.wiebe@johnstongroup.ca>
2015-09-09 08:25:39 -0500

    add place holder for GRANT

--- flyway-core/src/main/resources/org/flywaydb/core/internal/dbsupport/db2iseries/createMetaDataTable.sql
+++ flyway-core/src/main/resources/org/flywaydb/core/internal/dbsupport/db2iseries/createMetaDataTable.sql
@@ -32,3 +32,5 @@ ALTER TABLE ${schema}.${table} ADD CONSTRAINT ${schema}.${table}_pk PRIMARY KEY 
 CREATE INDEX ${schema}.${table}_vr_idx ON ${schema}.${table} (version_rank);
 CREATE INDEX ${schema}.${table}_ir_idx ON ${schema}.${table} (installed_rank);
 CREATE INDEX ${schema}.${table}_s_idx ON ${schema}.${table} (success);
+
+-- grant select, insert, update, delete on ${schema}.${table} to WHO?
